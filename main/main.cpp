267cd065827bd0f093b2b50887ba5c8d614bd707
vss <28676655+vss2sn@users.noreply.github.com>
2020-08-10 23:52:33 -0400

    Add time test setup for sorting algorithms

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,6 @@
 #include <future>
+#include <cassert>
+#include <chrono>
 #include <iostream>
 #include <memory>
 #include <random>
@@ -10,14 +12,32 @@
 #include "algorithms/sort.hpp"
 #include "algorithms/sort_iter.hpp"
 
+using namespace std::chrono;
+
 int main() {
-	std::vector<int> to_sort{10,8,9,3,5,6,1,4,2,7};
 
-	sort::SelectionSort(to_sort.begin(), to_sort.end());
+	const int size_to_sort{1000};
+  std::vector<int> to_sort(size_to_sort);
+  std::random_device rd;
 
-	for(const auto& ele : to_sort) {
-		std::cout << ele << ' ';
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dist(0, size_to_sort);
+	int duration = 0;
+
+	for(int i = 0; i < 1000; i++) {
+		std::cout << "Iteration: " << i << std::endl;
+		std::generate(to_sort.begin(), to_sort.end(), [&](){return dist(gen);});
+
+		auto start = high_resolution_clock::now();
+		// sort::InsertionSort(to_sort);
+		sort::InsertionSort(to_sort.begin(), to_sort.end());
+		auto stop = high_resolution_clock::now();
+
+		assert(std::is_sorted(to_sort.begin(), to_sort.end()));
+		duration += duration_cast<microseconds>(stop - start).count();
 	}
-	std::cout << '\n';
+
+	std::cout << "Time taken: " << duration << std::endl;
+
 	return 0;
 }
--- test/include/test/sorting_tests.hpp
+++ test/include/test/sorting_tests.hpp
@@ -9,17 +9,16 @@
 class SortingTestRandomInt : public ::testing::Test {
 protected:
   const int size_to_sort{10000};
-  std::vector<int> to_sort_stl, to_sort_algo;
+  std::vector<int> to_sort;
   std::random_device rd;
 
   SortingTestRandomInt()
-  :to_sort_stl(size_to_sort,0){}
+  :to_sort(size_to_sort,0){}
 
   void SetUp() override {
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dist(0, size_to_sort);
-    std::generate(to_sort_stl.begin(), to_sort_stl.end(), [&](){return dist(gen);});
-    to_sort_algo = to_sort_stl;
+    std::generate(to_sort.begin(), to_sort.end(), [&](){return dist(gen);});
   }
 };
 
