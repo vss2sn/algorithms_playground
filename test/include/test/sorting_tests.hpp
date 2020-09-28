db018f15ec44448b3f0382dd5a723ebb56fc90e0
vss <28676655+vss2sn@users.noreply.github.com>
2020-09-28 14:11:57 -0400

    Add templated tests for sorting and travis CI file

--- test/include/test/sorting_tests.hpp
+++ test/include/test/sorting_tests.hpp
@@ -8,7 +8,7 @@
 
 class SortingTestRandomInt : public ::testing::Test {
 protected:
-  const int size_to_sort{10000};
+  const int size_to_sort{100};
   std::vector<int> to_sort;
   std::random_device rd;
 
@@ -19,6 +19,26 @@ protected:
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dist(0, size_to_sort);
     std::generate(to_sort.begin(), to_sort.end(), [&](){return dist(gen);});
+  }
+};
+
+class SortingTestParamInt : public ::testing::TestWithParam<std::vector<int>>
+{
+public:
+  std::vector<int> to_sort;
+
+  void SetUp() override {
+    to_sort = GetParam();
+  }
+};
+
+class SortingTestParamChar : public ::testing::TestWithParam<std::vector<char>>
+{
+public:
+  std::vector<char> to_sort;
+
+  void SetUp() override {
+    to_sort = GetParam();
   }
 };
 
--- test/src/sorting_tests.cpp
+++ test/src/sorting_tests.cpp
@@ -117,6 +117,254 @@ TEST_F(SortingTestRandomInt, STLSort) {
   ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
 }
 
+TEST_P(SortingTestParamInt, InsertionSort) {
+  sort::InsertionSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, InsertionSortWithCb) {
+  sort::InsertionSortWithCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, InsertionSortWithRiCb) {
+  sort::InsertionSortWithRiCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, InsertionSortWithBinarySearch) {
+  sort::InsertionSortWithBinarySearch(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, InsertionSortWithBinarySearchWithCb) {
+  sort::InsertionSortWithBinarySearchWithCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, InsertionSortOptimized) {
+  sort::InsertionSortOptimized(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, InsertionSortIter) {
+  sort::InsertionSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, InsertionSortOptimizedWithCb) {
+  sort::InsertionSortOptimizedWithCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, BubbleSort) {
+  sort::BubbleSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, BubbleSortIter) {
+  sort::BubbleSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, MergeSort) {
+  sort::MergeSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, MergeSortIter) {
+  sort::MergeSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, MergeSortInPlace) {
+  sort::MergeSortInPlace(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, MergeSortInPlaceIter) {
+  sort::MergeSortInPlace(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, QuickSort) {
+  sort::QuickSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, QuickSortIter) {
+  sort::QuickSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, SelectionSort) {
+  sort::SelectionSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, SelectionSortIter) {
+  sort::SelectionSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, BucketSort) {
+  sort::BucketSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, CountingSort) {
+  sort::CountingSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, HeapSort) {
+  sort::HeapSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, HeapSortIter) {
+  sort::HeapSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamInt, STLSort) {
+  std::sort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+INSTANTIATE_TEST_CASE_P(SortTest, SortingTestParamInt,
+  ::testing::Values(std::vector<int>{0, 0, 0, 0, 0},
+                    std::vector<int>{5, 4, 3, 2, 1},
+                    std::vector<int>{1, 2, 3, 4, 5},
+                    std::vector<int>{1, 2, 3, 2, 1},
+                    std::vector<int>{1, 2, 3, 5, 4},
+                    std::vector<int>{1, 2, 3, 4, 5, 6},
+                    std::vector<int>{6, 5, 4, 3, 2, 1},
+                    std::vector<int>{1, 2, 3, 3, 2, 1},
+                    std::vector<int>{1, 2, 3, 4, 6, 5}));
+
+TEST_P(SortingTestParamChar, InsertionSort) {
+  sort::InsertionSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, InsertionSortWithCb) {
+  sort::InsertionSortWithCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, InsertionSortWithRiCb) {
+  sort::InsertionSortWithRiCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, InsertionSortWithBinarySearch) {
+  sort::InsertionSortWithBinarySearch(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, InsertionSortWithBinarySearchWithCb) {
+  sort::InsertionSortWithBinarySearchWithCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, InsertionSortOptimized) {
+  sort::InsertionSortOptimized(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, InsertionSortIter) {
+  sort::InsertionSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, InsertionSortOptimizedWithCb) {
+  sort::InsertionSortOptimizedWithCb(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, BubbleSort) {
+  sort::BubbleSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, BubbleSortIter) {
+  sort::BubbleSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, MergeSort) {
+  sort::MergeSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, MergeSortIter) {
+  sort::MergeSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, MergeSortInPlace) {
+  sort::MergeSortInPlace(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, MergeSortInPlaceIter) {
+  sort::MergeSortInPlace(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, QuickSort) {
+  sort::QuickSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, QuickSortIter) {
+  sort::QuickSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, SelectionSort) {
+  sort::SelectionSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, SelectionSortIter) {
+  sort::SelectionSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, BucketSort) {
+  sort::BucketSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, CountingSort) {
+  sort::CountingSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, HeapSort) {
+  sort::HeapSort(to_sort);
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, HeapSortIter) {
+  sort::HeapSort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+TEST_P(SortingTestParamChar, STLSort) {
+  std::sort(to_sort.begin(), to_sort.end());
+  ASSERT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
+INSTANTIATE_TEST_CASE_P(SortTest, SortingTestParamChar,
+  ::testing::Values(std::vector<char>{'a', 'b', 'c', 'd', 'e'},
+                    std::vector<char>{'e', 'd', 'c', 'b', 'a'},
+                    std::vector<char>{'a', 'a', 'a', 'a', 'a'},
+                    std::vector<char>{'a', 'b', 'c', 'b', 'a'}
+                  ));
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
